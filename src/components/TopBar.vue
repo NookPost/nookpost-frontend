4a0dc3aec42e73cf6d24531fa7ac20910e8291a3
Jannik-Hm <jannik.heym@gmx.de>
2025-02-03 20:34:54 +0100

    login page + toasts for login fail + successful logout

--- src/components/TopBar.vue
+++ src/components/TopBar.vue
@@ -1,14 +1,19 @@
 <script setup lang="ts">
-import { Menubar, Button, TieredMenu } from 'primevue'
+import { Menubar, Button, TieredMenu, useToast } from 'primevue'
 import type { MenuItem } from 'primevue/menuitem'
 import SearchBar from './SearchBar.vue'
 import { authStore } from '@/store/auth'
 import { ref } from 'vue'
+import { useRouter } from 'vue-router'
+const router = useRouter()
+const toast = useToast();
+
 interface Page {
   display_name: string
   path: string
   icon?: string | undefined
 }
+
 const pages: Page[] = [
   {
     display_name: 'Home',
@@ -41,6 +46,7 @@ const avatarMenu = ref(<MenuItem[]>[
     icon: 'pi pi-sign-out',
     command: () => {
       auth.logout()
+      toast.add({ severity: 'info', summary: 'Info', detail: 'Logout successful', life: 3000, group: 'top-right' });
     },
   },
 ])
@@ -64,7 +70,7 @@ const avatarMenu = ref(<MenuItem[]>[
     <template #end>
       <div class="toolbar-inline">
         <SearchBar></SearchBar>
-        <Button v-if="!auth.isLoggedIn" v-on:click="auth.login()">Login</Button>
+        <Button v-if="!auth.isLoggedIn" v-on:click="router.push('/login')">Login</Button>
         <span v-else>
           <Button icon="pi pi-user" @click="toggle" variant="outlined"></Button>
           <TieredMenu ref="menu" id="overlay_tmenu" :model="avatarMenu" popup />
--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,3 +1,74 @@
+<script setup lang="ts">
+import { Form, type FormSubmitEvent } from '@primevue/forms'
+import { InputText, Password, Button, useToast } from 'primevue'
+import { toRaw, ref } from 'vue'
+import { authStore } from '@/store/auth'
+import { useRouter } from 'vue-router'
+
+const isLoading = ref(false);
+
+const router = useRouter();
+
+const toast = useToast();
+
+async function onFormSubmit(event: FormSubmitEvent) {
+  isLoading.value = true;
+  const formData = toRaw(event.states)
+  const auth = authStore()
+  const success: boolean = await auth.login({
+    username: formData.username.value,
+    password: formData.password.value,
+  })
+  if(success) {
+    router.push('/');
+  } else {
+    isLoading.value = false;
+    toast.add({ severity: 'error', summary: 'Error', detail: 'Login failed', life: 3000, group: 'bottom-center' });
+  }
+}
+</script>
+
 <template>
-  <div></div>
+  <div class="form-center outer">
+    <div class="form-center inner">
+      <Form class="flex flex-col gap-4 fit" @submit="onFormSubmit">
+        <img src="/src/assets/logo.svg" />
+        <InputText name="username" placeholder="Username" />
+        <Password name="password" placeholder="Password" :feedback="false" />
+        <Button type="submit" :is-loading="isLoading" :loading="isLoading" label="Submit" />
+      </Form>
+    </div>
+    <RouterLink to="/register">Register</RouterLink>
+  </div>
 </template>
+
+<style scoped>
+.flex {
+  display: flex;
+}
+.flex-col {
+  flex-direction: column;
+}
+.gap-1 {
+  gap: 0.25rem;
+}
+.gap-4 {
+  gap: 1rem;
+}
+.form-center.outer {
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+.form-center.inner {
+  border: 1px solid var(--p-inputtext-border-color);
+  border-radius: var(--p-inputtext-border-radius);
+  padding: 25px;
+  background-color: var(--vt-c-divider-dark-2);
+}
+a:hover {
+  background-color: transparent;
+}
+</style>

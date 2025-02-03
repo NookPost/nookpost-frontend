023420a59815ca03824c2770bcb739152239ad98
Jannik-Hm <jannik.heym@gmx.de>
2025-02-03 20:34:17 +0100

    central API config

---
+++ src/util/api.ts
@@ -0,0 +1,28 @@
+import { Configuration } from '@/api'
+import { authStore } from '@/store/auth'
+
+export function getAPIConfig(withAPIKey: boolean = true): Configuration {
+  if (withAPIKey) {
+    const authData = authStore()
+    return new Configuration({
+      basePath: 'http://localhost:5001',
+      accessToken: authData.getAccessToken() ?? '',
+      baseOptions: {
+        headers: {
+          // override to undefined as by default it is set to 'OpenAPI-Generator/typescript-axios'
+          'User-Agent': undefined,
+        },
+      },
+    })
+  } else {
+    return new Configuration({
+      basePath: 'http://localhost:5001',
+      baseOptions: {
+        headers: {
+          // override to undefined as by default it is set to 'OpenAPI-Generator/typescript-axios'
+          'User-Agent': undefined,
+        },
+      },
+    })
+  }
+}

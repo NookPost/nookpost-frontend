c90562b653df60cb510f404fd470e90f8b9bc2e7
Jannik-Hm <jannik.heym@gmx.de>
2025-02-04 23:49:46 +0100

    sample on how to use id in url in post detail page

--- src/views/PostDetailView.vue
+++ src/views/PostDetailView.vue
@@ -1,3 +1,9 @@
+<script setup lang="ts">
+import { useRoute } from 'vue-router'
+const route = useRoute()
+const id = route.params.id
+</script>
+
 <template>
-  <div></div>
+  <div>This will later be the detail page for post {{ id }}</div>
 </template>

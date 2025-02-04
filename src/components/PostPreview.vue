e36cdc06150c0e126b141538a577b83666072b6d
Jannik-Hm <jannik.heym@gmx.de>
2025-02-04 20:16:02 +0100

    added PostPreview Component

---
+++ src/components/PostPreview.vue
@@ -0,0 +1,73 @@
+<script setup lang="ts">
+import { Tag } from 'primevue'
+import { ref } from 'vue'
+
+const readMore = ref(false)
+
+function getShortenedBody(input: string): string {
+  const numberOfChars: number = 250
+  if (input.length > numberOfChars) {
+    readMore.value = true
+  }
+  const temp: string = input.slice(0, numberOfChars - 1)
+  return temp.substring(0, temp.lastIndexOf(' '))
+}
+</script>
+
+<style scoped src="/src/assets/post.css" />
+
+<template>
+  <div class="post-border">
+    <h1 class="post-title">{{ title }}</h1>
+    <div class="post-meta">
+      <h2>
+        created&nbsp;by
+        <span class="post-author">{{ author.replace(' ', '&nbsp;') }}</span> on&nbsp;<span
+          class="post-created"
+          >{{ new Date(created * 1000).toLocaleDateString() }}</span
+        >
+      </h2>
+      <div class="post-tags">
+        <Tag class="post-category">{{ category }}</Tag>
+      </div>
+    </div>
+    <div class="post-body">
+      <p>
+        {{ getShortenedBody(body) }}
+        <span v-if="readMore" class="post-readmore">[...]</span>
+      </p>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+    body: {
+      type: String,
+      required: true,
+    },
+    author: {
+      type: String,
+      required: true,
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    created: {
+      type: Number,
+      required: true,
+    },
+    modified: {
+      type: Number,
+      default: 0,
+      required: false,
+    },
+  },
+}
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,17 @@
 <script setup lang="ts">
 import TheWelcome from '../components/TheWelcome.vue'
+import PostPreview from '@/components/PostPreview.vue'
 </script>
 
 <template>
   <main>
     <TheWelcome />
+    <PostPreview
+      title="Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam"
+      author="Jack Sparrow"
+      body="Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet."
+      category="Technology"
+      :created="1738692463"
+    ></PostPreview>
   </main>
 </template>

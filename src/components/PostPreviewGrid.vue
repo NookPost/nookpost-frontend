d1c83d8f8eac9419e0d0f1de3217a0f91708735c
Jannik-Hm <jannik.heym@gmx.de>
2025-02-05 09:20:07 +0100

    post preview grid now takes in list of Posts as a prop and generates post preview in for each manner

--- src/components/PostPreviewGrid.vue
+++ src/components/PostPreviewGrid.vue
@@ -1,37 +1,29 @@
 <script setup lang="ts">
-import PostPreview from './PostPreview.vue';
+import type { PropType } from 'vue'
+import PostPreview from './PostPreview.vue'
+import type { Post } from '@/types/post'
 </script>
 
 <template>
   <div class="post-grid">
-      <PostPreview
-        uuid="adadasd3"
-        title="Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam"
-        author="Jack Sparrow"
-        body="Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet."
-        category="Technology"
-        :created="1738692463"
-      />
-      <PostPreview
-        uuid="adadasd2"
-        title="Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam"
-        author="Jack Sparrow"
-        body="Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet."
-        category="Technology"
-        :created="1738792463"
-      />
-      <PostPreview
-        uuid="adadasd"
-        title="Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam"
-        author="Jack Sparrow"
-        body="Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet."
-        category="Technology"
-        :created="1739692463"
-      />
-    </div>
+    <template v-for="post in posts" :key="post.uuid">
+      <PostPreview :post="post"></PostPreview>
+    </template>
+  </div>
 </template>
 
-<style scoped src="/src/assets/post-grid.css"/>
+<script lang="ts">
+export default {
+  props: {
+    posts: {
+      type: Object as PropType<Post[]>,
+      required: true,
+    },
+  },
+}
+</script>
+
+<style scoped src="/src/assets/post-grid.css" />
 
 <style scoped>
 a:hover {
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,41 @@
 <script setup lang="ts">
 import TheWelcome from '../components/TheWelcome.vue'
 import PostPreviewGrid from '@/components/PostPreviewGrid.vue';
+import type { Post } from '@/types/post';
+const posts: Post[] = [
+  {
+    uuid: 'adadasd3',
+    title: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam',
+    author: 'Jack Sparrow',
+    body: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.',
+    category: 'Technology',
+    created: 1738692463,
+    modified: 0,
+  },
+  {
+    uuid: 'adadasd',
+    title: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam',
+    author: 'Jack Sparrow',
+    body: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.',
+    category: 'Technology',
+    created: 1738792463,
+    modified: 0,
+  },
+  {
+    uuid: 'adadasd',
+    title: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam',
+    author: 'Jack Sparrow',
+    body: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.',
+    category: 'Technology',
+    created: 1739692463,
+    modified: 0,
+  },
+]
 </script>
 
 <template>
   <main>
     <TheWelcome />
-    <PostPreviewGrid/>
+    <PostPreviewGrid v-bind:posts="posts"/>
   </main>
 </template>

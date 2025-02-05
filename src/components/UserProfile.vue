1175ff7f2119e9c58a8f2c7a58cb09f269af6fe9
Jannik-Hm <jannik.heym@gmx.de>
2025-02-05 22:40:59 +0100

    displaying 3 last posts next to user profile data on profile page

--- src/components/UserProfile.vue
+++ src/components/UserProfile.vue
@@ -3,36 +3,70 @@ import type { PropType } from 'vue'
 import type { Profile } from '@/types/profile'
 import { Avatar, Button, Fieldset } from 'primevue'
 import { PrimeIcons } from '@primevue/core/api'
+import PostPreview from './PostPreview.vue'
+import type { Post } from '@/types/post'
+const posts: Post[] = [
+  {
+    uuid: 'adadasd3',
+    title: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam',
+    author: 'Jack Sparrow',
+    body: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.',
+    category: 'Technology',
+    created: 1738692463,
+    modified: 0,
+  },
+  {
+    uuid: 'adadasd',
+    title: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam',
+    author: 'Jack Sparrow',
+    body: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.',
+    category: 'Technology',
+    created: 1738792463,
+    modified: 0,
+  },
+  {
+    uuid: 'adadasd',
+    title: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam',
+    author: 'Jack Sparrow',
+    body: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.',
+    category: 'Technology',
+    created: 1739692463,
+    modified: 0,
+  },
+]
 </script>
 
 <template>
-  <div class="profile-border">
-    <div class="profile-avatar">
-      <Avatar :image="profile.profilePictureBase64" shape="circle" size="xlarge" />
-    </div>
-    <div class="profile-head">
-      <h1 class="profile-username">
-        {{ profile.displayname }}
-      </h1>
-      <Button
-        :icon="PrimeIcons.ENVELOPE"
-        variant="link"
-        as="a"
-        :href="'mailto:' + profile.email"
-        rounded
-        severity="secondary"
-      />
-    </div>
-    <div>
+  <span class="profile-grid">
+    <div class="profile-border">
+      <div class="profile-avatar">
+        <Avatar :image="profile.profilePictureBase64" shape="circle" size="xlarge" />
+      </div>
+      <div class="profile-head">
+        <h1 class="profile-username">
+          {{ profile.displayname }}
+        </h1>
+        <Button
+          :icon="PrimeIcons.ENVELOPE"
+          variant="link"
+          as="a"
+          :href="'mailto:' + profile.email"
+          rounded
+          severity="secondary"
+        />
+      </div>
       <h2>{{ profile.username }}</h2>
-    </div>
-    <div>
       <Fieldset legend="Tagline">{{ profile.tagline }}</Fieldset>
-    </div>
-    <div>
       <Fieldset legend="Bio">{{ profile.bio }}</Fieldset>
     </div>
-  </div>
+    <div class="profile-post-preview profile-border">
+      <!-- Display last 3 Posts -->
+      <template v-for="post in posts.slice(0, 3)" :key="post.uuid">
+        <PostPreview :post="post"></PostPreview>
+      </template>
+      <h3 class="no-posts-yet" v-if="posts.length == 0">This user has not shared any posts yet</h3>
+    </div>
+  </span>
 </template>
 
 <script lang="ts">

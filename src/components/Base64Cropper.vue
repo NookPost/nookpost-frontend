2c34546a2ad3326ded6ef27c6aff13a4c39babed
Jannik-Hm <jannik.heym@gmx.de>
2025-02-05 16:35:18 +0100

    added post detail component and vue + crop component (for testing purpose part of post detail)

---
+++ src/components/Base64Cropper.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+import { Cropper } from 'vue-advanced-cropper';
+import 'vue-advanced-cropper/dist/style.css';
+
+function onCrop({ canvas }: { canvas: HTMLCanvasElement }): string {
+  console.log(canvas)
+  return canvas.toDataURL("image/jpeg")
+}
+</script>
+
+<template>
+  <Cropper
+    class="cropper"
+    :src="source"
+    :stencil-props="{
+      aspectRatio: 32/9
+    }"
+    :resizeImage="false"
+    @change="$emit('update:base64Result', onCrop($event))"
+    />
+</template>
+
+<script lang="ts">
+export default {
+  props: {
+    source: {
+      type: String,
+      required: true,
+    },
+    base64Result: {
+      required: true,
+    }
+  },
+  emits: ['update:base64Result']
+}
+</script>
+
+<style>
+.cropper {
+  max-height: 300px;
+}
+</style>
---
+++ src/components/PostDetail.vue
@@ -0,0 +1,48 @@
+<script setup lang="ts">
+import { Tag, Image } from 'primevue'
+import { ref, type PropType } from 'vue'
+import type { Post } from '@/types/post'
+import Base64Cropper from './Base64Cropper.vue';
+
+const croppedBase64 = ref("");
+
+</script>
+
+<style scoped src="/src/assets/post.css" />
+
+<template>
+  <div class="post-border detail">
+    <h1 class="post-title">{{ post.title }}</h1>
+    <div class="post-meta detail">
+      <h2>
+        created&nbsp;by
+        <span class="post-author">{{ post.author.replace(' ', '&nbsp;') }}</span> on&nbsp;<span
+          class="post-created"
+          >{{ new Date(post.created * 1000).toLocaleDateString() }}</span
+        >
+      </h2>
+      <div class="post-tags">
+        <Tag class="post-category">{{ post.category }}</Tag>
+      </div>
+    </div>
+    <Base64Cropper v-model:base64-result="croppedBase64" source="https://images.pexels.com/photos/226746/pexels-photo-226746.jpeg"/>
+    <!-- 32:9 Image ratio seems good -->
+    <Image v-bind:src="croppedBase64" alt="Image" class="post-banner" preview />
+    <div class="post-body">
+      <p>
+        {{ post.body }}
+      </p>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  props: {
+    post: {
+      type: Object as PropType<Post>,
+      required: true,
+    },
+  },
+}
+</script>

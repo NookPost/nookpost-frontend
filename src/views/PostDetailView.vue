30bb8bede61805d90db6f058bbd74c522be332ef
Jannik-Hm <jannik.heym@gmx.de>
2025-02-07 15:24:52 +0100

    first version of banner uploader and crop

--- src/views/PostDetailView.vue
+++ src/views/PostDetailView.vue
@@ -9,7 +9,7 @@ const post: Post = {
   title: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam',
   author: 'Jack Sparrow',
   body: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.',
-  category: 'Technology',
+  categoryUUID: '35dbaec3-6738-42e5-bfd8-79e5877e3ffd',
   created: 1739692463,
   modified: 0,
 }
@@ -18,7 +18,7 @@ const post: Post = {
 <template>
   <div class="post-detail-view">
     This will later be the detail page for post {{ id }}
-    <PostDetail :post="post" />
+    <PostDetail :init-values="post" :edit="true" />
   </div>
 </template>
 

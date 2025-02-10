f3d2666a22903f7911ba98c1c1b26847ad76f6ce
LightJack05 <66321084+LightJack05@users.noreply.github.com>
2025-02-10 15:19:34 +0100

    Refactored some login stuff

--- src/store/auth.ts
+++ src/store/auth.ts
@@ -13,14 +13,9 @@ import { AxiosError, type AxiosResponse } from 'axios'
 
 // private Store
 const authData = defineStore('authData', () => {
-  const loggedIn: Ref<boolean, boolean> = ref(false)
   const accessToken: Ref<string | null, string | null> = ref(null)
   const expiryTimestamp: Ref<number | null, number | null> = ref(null)
 
-  if (localStorage.getItem('loggedIn')) {
-    loggedIn.value = localStorage.getItem('loggedIn') === 'true'
-  }
-  // TODO: find proper solution to safely store access token across page reloads
   if (localStorage.getItem('accessToken')) {
     accessToken.value = localStorage.getItem('accessToken')
   }
@@ -28,24 +23,13 @@ const authData = defineStore('authData', () => {
     expiryTimestamp.value = Number(localStorage.getItem('expiryTimestamp'))
   }
 
-  return { loggedIn, accessToken, expiryTimestamp }
+  return { accessToken, expiryTimestamp }
 })
 
-interface loginData {
-  username: string
-  password: string
-}
-
-interface loginResponseData {
-  accessToken: string
-  expireTimestamp: number
-}
-
-function saveLoginResponseData({ accessToken, expireTimestamp }: loginResponseData) {
+function saveLoginResponseData(accessToken: string, expireTimestamp: number) {
   const data = authData()
-  data.loggedIn = true
-  localStorage.setItem('loggedIn', String(true))
-  // TODO: find proper solution to safely store access token across page reloads
+  data.accessToken = accessToken
+  data.expiryTimestamp = expireTimestamp
   localStorage.setItem('accessToken', accessToken)
   localStorage.setItem('expiryTimestamp', String(expireTimestamp))
 }
@@ -53,7 +37,7 @@ function saveLoginResponseData({ accessToken, expireTimestamp }: loginResponseDa
 export const authStore = defineStore('authStore', {
   state: () => {
     const data = authData()
-    const isLoggedIn = computed(() => data.loggedIn)
+    const isLoggedIn = computed(() => Boolean(data.accessToken))
     const expireTime = computed(() =>
       // convert timestamp in seconds to milliseconds
       data.expiryTimestamp != null ? new Date(data.expiryTimestamp * 1000) : null,
@@ -62,7 +46,7 @@ export const authStore = defineStore('authStore', {
     return { isLoggedIn, expireTime }
   },
   actions: {
-    async register({ username, password }: loginData): Promise<boolean> {
+    async register(username: string, password: string): Promise<boolean> {
       const configuration = getAPIConfig(false)
       const authAPI = new UsersApi(configuration)
       let response: AxiosResponse<UsersPostResponseBody, unknown>
@@ -79,10 +63,7 @@ export const authStore = defineStore('authStore', {
         }
       }
       if (response != undefined && response.status === 200 && response.statusText === 'OK') {
-        saveLoginResponseData({
-          accessToken: response.data.token ?? '',
-          expireTimestamp: response.data.expiryTimestamp ?? 0,
-        })
+        saveLoginResponseData(response.data.token ?? '', response.data.expiryTimestamp ?? 0)
         return true
       } else {
         // error handling
@@ -90,7 +71,7 @@ export const authStore = defineStore('authStore', {
       }
     },
 
-    async login({ username, password }: loginData): Promise<boolean> {
+    async login(username: string, password: string): Promise<boolean> {
       const configuration = getAPIConfig(false)
       const authAPI = new AuthenticationApi(configuration)
       let response: AxiosResponse<AuthenticationLoginResponseBody, unknown>
@@ -107,10 +88,7 @@ export const authStore = defineStore('authStore', {
         }
       }
       if (response != undefined && response.status === 200 && response.statusText === 'OK') {
-        saveLoginResponseData({
-          accessToken: response.data.token ?? '',
-          expireTimestamp: response.data.expiryTimestamp ?? 0,
-        })
+        saveLoginResponseData(response.data.token ?? '', response.data.expiryTimestamp ?? 0)
         return true
       } else {
         // error handling
@@ -120,11 +98,8 @@ export const authStore = defineStore('authStore', {
 
     logout() {
       const data = authData()
-      localStorage.setItem('loggedIn', String(false))
-      // TODO: find proper solution to safely store access token across page reloads
       localStorage.removeItem('accessToken')
       localStorage.removeItem('expiryTimestamp')
-      data.loggedIn = false
       data.accessToken = null
       data.expiryTimestamp = null
     },

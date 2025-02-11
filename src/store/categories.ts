397db4dab1a982ff07f234ee7be3f5dd6b7ebc15
LightJack05 <66321084+LightJack05@users.noreply.github.com>
2025-02-11 19:46:00 +0100

    Removed unused import

--- src/store/categories.ts
+++ src/store/categories.ts
@@ -1,5 +1,5 @@
 import { defineStore } from 'pinia'
-import { computed, ref, type Ref } from 'vue'
+import { ref, type Ref } from 'vue'
 import { getAPIConfig } from '@/util/api'
 import { AxiosError, type AxiosResponse } from 'axios'
 import type { Category } from '@/types/category'

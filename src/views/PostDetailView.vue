963c310476790d29a312e56da9b386e486d1cbfa
Jannik-Hm <jannik.heym@gmx.de>
2025-02-12 10:23:07 +0100

    added loading spinner to post detail view + post edit to router

--- src/views/PostDetailView.vue
+++ src/views/PostDetailView.vue
@@ -4,13 +4,18 @@ import PostDetail from '@/components/PostDetail.vue'
 import type { Post } from '@/types/post'
 import { onMounted, ref, type Ref } from 'vue'
 import { fetchPost } from '@/view-api-interaction/PostDetailView'
+import { ProgressSpinner } from 'primevue'
+import { categoryData } from '@/store/categories'
 const route = useRoute()
 let id = route.params.id
+const edit: boolean = (route.meta.edit as boolean) ?? false
 const post: Ref<Post | null> = ref(null)
 
 if (id instanceof Array) {
   id = id[0]
 }
+
+const categories = categoryData()
 
 onMounted(() => {
   fetchPost(id).then((p) => (post.value = p))
@@ -19,21 +24,23 @@ onMounted(() => {
 
 <template>
   <div class="post-detail-view">
-    This will later be the detail page for post {{ id }}
-    <!-- View sample -->
-    <PostDetail v-if="post" :data="post" />
-    <!-- Edit sample (for creation pass empty object) -->
-    <!-- <PostDetail
-      v-model:data="post"
-      :edit="true"
-      v-on:update:post="console.log"
-      :categories="categories"
-    /> -->
+    <!-- When edit is false, then display  -->
+    <PostDetail
+    v-if="post"
+    v-model:data="post"
+    :edit="edit"
+    v-on:update:post="console.log"
+    :categories="categories.categories"
+    />
+    <ProgressSpinner v-else />
   </div>
 </template>
 
 <style scoped>
 .post-detail-view {
+  display: flex;
+  align-items: center;
+  justify-content: center;
   margin: 0 auto;
   width: clamp(400px, 90%, 1600px);
 }

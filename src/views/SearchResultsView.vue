d0952415b4fe8830af310b9ba16294a6c07a9b9a
Jannik-Hm <jannik.heym@gmx.de>
2025-02-23 20:06:06 +0100

    created searchresultsview

--- src/views/SearchResultsView.vue
+++ src/views/SearchResultsView.vue
@@ -1,3 +1,26 @@
+<script setup lang="ts">
+import PostPreviewGrid from '@/components/PostPreviewGrid.vue'
+import { categoryData } from '@/store/categories'
+import type { Post } from '@/types/post'
+import { onMounted, ref, type Ref } from 'vue'
+import { fetchPosts } from '@/view-api-interaction/HomeView'
+const posts: Ref<Post[]> = ref([])
+
+const categoryStore = categoryData()
+
+const params: URLSearchParams = new URLSearchParams(window.location.search);
+
+onMounted(() => {
+  const searchstring = params.get("q")
+  console.log(searchstring)
+  categoryStore.loadCategories().then(() => {
+    fetchPosts().then((p) => (posts.value = p))
+  })
+})
+</script>
+
 <template>
-  <div></div>
+  <div>
+    <PostPreviewGrid v-bind:posts="posts" />
+  </div>
 </template>

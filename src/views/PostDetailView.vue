671a15b405211c702521e96007942b260798f5c1
LightJack05 <66321084+LightJack05@users.noreply.github.com>
2025-02-24 19:37:41 +0100

    Made sure post edit mode is updated properly

--- src/views/PostDetailView.vue
+++ src/views/PostDetailView.vue
@@ -2,18 +2,21 @@
 import { useRoute } from 'vue-router'
 import PostDetail from '@/components/PostDetail.vue'
 import type { Post } from '@/types/post'
-import { onMounted, ref, type Ref } from 'vue'
+import { onMounted, ref, watch, type Ref } from 'vue'
 import { fetchPost, updatePost } from '@/view-api-interaction/PostDetailView'
 import { ProgressSpinner } from 'primevue'
 import { categoryData } from '@/store/categories'
 const route = useRoute()
-let id = route.params.id
+let id: string
 const edit: Ref<boolean, boolean> = ref((route.meta.edit as boolean | undefined) ?? false)
 const create: boolean = (route.meta.create as boolean | undefined) ?? false
 const post: Ref<Post | null> = ref(null)
 
-if (id instanceof Array) {
-  id = id[0]
+
+if (route.params.id instanceof Array) {
+  id = route.params.id[0]
+} else {
+  id = route.params.id
 }
 
 if (create) {
@@ -33,35 +36,34 @@ if (create) {
 
 const categories = categoryData()
 
+watch(
+  () => route.meta.edit,
+  () => onViewLoaded()
+)
+
 onMounted(() => {
+  onViewLoaded()
+})
+
+function onViewLoaded() {
   edit.value = (route.meta.edit as boolean | undefined) ?? false
   categories.loadCategories().then(() => {
     if (!create) {
       fetchPost(id).then((p) => (post.value = p))
     }
   })
-})
+}
 
 function onEditPost(editedPost: Post) {
-  updatePost(editedPost).then(() => {
-    edit.value = false
-    fetchPost(editedPost.uuid).then((p) => {
-      post.value = p
-    })
-  })
+  updatePost(editedPost)
 }
 </script>
 
 <template>
   <div class="post-detail-view">
     <!-- When edit is false, then display  -->
-    <PostDetail
-      v-if="post"
-      v-model:data="post"
-      :edit="edit"
-      v-on:update:post="onEditPost"
-      :categories="categories.categories"
-    />
+    <PostDetail v-if="post" v-model:data="post" :edit="edit" v-on:update:post="onEditPost"
+      :categories="categories.categories" />
     <ProgressSpinner v-else />
   </div>
 </template>

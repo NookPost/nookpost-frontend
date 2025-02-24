2ed4a2454cf31c4995308cd47eac96e819c5b4b0
LightJack05 <66321084+LightJack05@users.noreply.github.com>
2025-02-24 22:11:36 +0100

    Connected search view to API

--- src/views/SearchResultsView.vue
+++ src/views/SearchResultsView.vue
@@ -2,21 +2,33 @@
 import PostPreviewGrid from '@/components/PostPreviewGrid.vue'
 import { categoryData } from '@/store/categories'
 import type { Post } from '@/types/post'
-import { onMounted, ref, type Ref } from 'vue'
-import { fetchPosts } from '@/view-api-interaction/HomeView'
+import { onMounted, ref, watch, type Ref } from 'vue'
+import { fetchPostsByTextSearch } from '@/view-api-interaction/SearchResultsView'
+import { useRoute } from 'vue-router'
 const posts: Ref<Post[]> = ref([])
+const route = useRoute()
 
 const categoryStore = categoryData()
 
 const params: URLSearchParams = new URLSearchParams(window.location.search)
 
+watch(
+  () => route.query.q,
+  () => onLoaded()
+)
+
 onMounted(() => {
-  const searchstring = params.get('q')
+  onLoaded()
+})
+
+function onLoaded() {
+  const searchstring = route.query.q instanceof Array ? route.query.q[0]?.toString() :
+    route.query.q?.toString()
   console.log(searchstring)
   categoryStore.loadCategories().then(() => {
-    fetchPosts().then((p) => (posts.value = p))
+    fetchPostsByTextSearch(searchstring).then((p) => (posts.value = p))
   })
-})
+}
 </script>
 
 <template>

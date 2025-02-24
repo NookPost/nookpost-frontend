725acebcc593fd83cd03a86e703f481366e559b2
LightJack05 <66321084+LightJack05@users.noreply.github.com>
2025-02-24 21:28:53 +0100

    Connected own user profile to fetch profile from API

--- src/views/PersonalProfileView.vue
+++ src/views/PersonalProfileView.vue
@@ -4,67 +4,44 @@ import type { Profile } from '@/types/profile'
 import UserProfile from '@/components/UserProfile.vue'
 import type { Category } from '@/types/category'
 import type { Post } from '@/types/post'
+import { categoryData } from '@/store/categories'
+import { onMounted, ref, type Ref } from 'vue'
+import { fetchMeUserProfile } from '@/view-api-interaction/PersonalProfileView'
+import { fetchPostsByUser } from '@/view-api-interaction/UserProfile'
 const route = useRoute()
-const id = route.params.username
+let id = route.params.username
 
-const profile: Profile = {
-  username: 'Test_1234',
-  displayname: 'Teee Rock Bottom',
-  email: 'test@test.com',
-  tagline: 'Wassup',
-  bio: 'Insert something creative here. Are you really reading this? Stop it. I said STOP!!! Well, since you are still reading, I give up... Have a nice evening.',
-  profilePictureBase64: 'https://images.pexels.com/photos/226746/pexels-photo-226746.jpeg',
+const categoryStore = categoryData()
+
+const profile: Ref<Profile | null> = ref(null)
+const categories: Ref<Category[]> = ref([])
+const posts: Ref<Post[]> = ref([])
+
+if (id instanceof Array) {
+  id = id[0]
 }
-const categories: Category[] = [
-  {
-    name: 'Technology',
-    uuid: '35dbaec3-6738-42e5-bfd8-79e5877e3ffd',
-    icon: 'pi pi-microchip',
-  },
-]
-const posts: Post[] = [
-  {
-    uuid: 'adadasd3',
-    title: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam',
-    authorUsername: 'Test_1234',
-    authorDisplayname: 'Teee Rock Bottom',
-    body: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.',
-    category: categories[0],
-    created: 1738692463,
-    modified: 0,
-    bannerImageBase64: '',
-  },
-  {
-    uuid: 'adadasd',
-    title: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam',
-    authorUsername: 'Test_1234',
-    authorDisplayname: 'Teee Rock Bottom',
-    body: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.',
-    category: categories[0],
-    created: 1738792463,
-    modified: 0,
-    bannerImageBase64: '',
-  },
-  {
-    uuid: 'adadasd',
-    title: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam',
-    authorUsername: 'Test_1234',
-    authorDisplayname: 'Teee Rock Bottom',
-    body: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.',
-    category: categories[0],
-    created: 1739692463,
-    modified: 0,
-    bannerImageBase64: '',
-  },
-]
+
+onMounted(() => {
+  categoryStore.loadCategories().then(() => {
+    categories.value = categoryStore.categories
+    fetchMeUserProfile().then((u) => {
+      profile.value = u
+      fetchPostsByUser(u?.username ?? '').then((p) => {
+        posts.value = p
+      })
+    })
+  })
+})
 </script>
 
 <template>
-  <div>User {{ id }}</div>
   <UserProfile
+    v-if="profile"
     :profile="profile"
     :posts="posts"
     :editable="true"
     @update:profile="console.log"
   ></UserProfile>
+  <!--TODO: Center this!-->
+  <ProgressSpinner v-else />
 </template>

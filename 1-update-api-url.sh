e7da548ecd8c1d814284ffbca041a440670c76d6
jkulzer <git@jkulzer.dev>
2025-02-24 14:00:45 +0100

    more debugging for env variable in docker

--- 1-update-api-url.sh
+++ 1-update-api-url.sh
@@ -3,6 +3,7 @@ if [ -z ${BACKEND_URL+x} ]; then
   echo "BACKEND_URL variable is not set" && exit
 else
   ESCAPED_URL=$(printf '%s\n' "$BACKEND_URL" | sed 's/[\/&]/\\&/g')
+	echo "Escaped URL is ${ESCAPED_URL}"
   find /usr/share/nginx/html -type f -exec sed -i "s|http://localhost:5001/|$ESCAPED_URL|g" {} +
 fi
 

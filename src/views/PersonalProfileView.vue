be554f96911a43b369a323056ed6147e91508e0c
LightJack05 <66321084+LightJack05@users.noreply.github.com>
2025-02-25 15:34:27 +0100

    Connected personal profile edit to API

--- src/views/PersonalProfileView.vue
+++ src/views/PersonalProfileView.vue
@@ -5,9 +5,11 @@ import UserProfile from '@/components/UserProfile.vue'
 import type { Category } from '@/types/category'
 import type { Post } from '@/types/post'
 import { categoryData } from '@/store/categories'
-import { onMounted, ref, type Ref } from 'vue'
-import { fetchMeUserProfile } from '@/view-api-interaction/PersonalProfileView'
+import { onMounted, ref, watch, type Ref } from 'vue'
+import { editMeUserProfile, fetchMeUserProfile } from '@/view-api-interaction/PersonalProfileView'
 import { fetchPostsByUser } from '@/view-api-interaction/UserProfile'
+import { authStore } from '@/store/auth'
+import router from '@/router'
 const route = useRoute()
 let id = route.params.username
 
@@ -17,11 +19,15 @@ const profile: Ref<Profile | null> = ref(null)
 const categories: Ref<Category[]> = ref([])
 const posts: Ref<Post[]> = ref([])
 
-if (id instanceof Array) {
-  id = id[0]
-}
 
 onMounted(() => {
+  onLoaded()
+})
+
+function onLoaded() {
+  if (id instanceof Array) {
+    id = id[0]
+  }
   categoryStore.loadCategories().then(() => {
     categories.value = categoryStore.categories
     fetchMeUserProfile().then((u) => {
@@ -31,7 +37,13 @@ onMounted(() => {
       })
     })
   })
-})
+}
+
+function onProfileEdit(user: Profile){
+  editMeUserProfile(user).then(() => {
+    onLoaded()
+  })
+}
 </script>
 
 <template>
@@ -40,7 +52,7 @@ onMounted(() => {
     :profile="profile"
     :posts="posts"
     :editable="true"
-    @update:profile="console.log"
+    @update:profile="onProfileEdit"
   ></UserProfile>
   <!--TODO: Center this!-->
   <ProgressSpinner v-else />

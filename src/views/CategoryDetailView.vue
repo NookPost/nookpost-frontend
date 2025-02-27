cf7ffae3b4cf96ae5de9277e358ba1b4f9307d83
LightJack05 <66321084+LightJack05@users.noreply.github.com>
2025-02-27 22:02:24 +0100

    Added error message on category not found

--- src/views/CategoryDetailView.vue
+++ src/views/CategoryDetailView.vue
@@ -7,6 +7,7 @@ import { useRoute } from 'vue-router'
 import type { Category } from '@/types/category'
 import { ProgressSpinner, useToast } from 'primevue'
 import { fetchPostsFiltered } from '@/api-calls/posts'
+import router from '@/router'
 const posts: Ref<Post[] | null> = ref(null)
 
 const categoryStore = categoryData()
@@ -26,6 +27,15 @@ onMounted(() => {
     category.value = categoryStore.categories.find((c) => c.uuid == id) ?? null
     if (category.value != null) {
       fetchPostsFiltered(toast, undefined, id).then((p) => (posts.value = p))
+    } else {
+      toast.add({
+        severity: 'error',
+        summary: 'Error',
+        detail: 'The requested category was not found.',
+        life: 3000,
+        group: 'top-right',
+      })
+      router.replace("/");
     }
   })
 })

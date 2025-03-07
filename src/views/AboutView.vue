9e7ff2e15946ecf43d30996669425dd28b264670
jkulzer <git@jkulzer.dev>
2025-03-08 00:04:31 +0100

    formatted

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,9 +1,9 @@
 <template>
   <h1>NookPost</h1>
   <p>
-    NookPost is the most amazing posting platform ever!
-    You can do things like making posts, editing them and maybe even reading them.
-    Writing this software was forced upon us and we are very sad about it :(
+    NookPost is the most amazing posting platform ever! You can do things like making posts, editing
+    them and maybe even reading them. Writing this software was forced upon us and we are very sad
+    about it :(
   </p>
 </template>
 
--- src/views/ImpressumView.vue
+++ src/views/ImpressumView.vue
@@ -1,7 +1,6 @@
 <template>
   <h1>Impressum</h1>
   <p>
-    Nookpost Developers
-    Contact: <a href="mailto:webmaster@jkulzer.dev">webmaster@jkulzer.dev</a>
+    Nookpost Developers Contact: <a href="mailto:webmaster@jkulzer.dev">webmaster@jkulzer.dev</a>
   </p>
 </template>

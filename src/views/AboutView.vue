1db7f454f724d1148a51d7e9ef6c0893bea7fad8
jkulzer <git@jkulzer.dev>
2025-03-07 15:15:05 +0100

    fix(static-pages): bigger font size

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,11 +1,10 @@
 <template>
   <h1>NookPost</h1>
-
-  NookPost is the most amazing posting platform ever!
-
-  You can do things like making posts, editing them and maybe even reading them.
-
-  Writing this software was forced upon us and we are very sad about it :(
+  <p>
+    NookPost is the most amazing posting platform ever!
+    You can do things like making posts, editing them and maybe even reading them.
+    Writing this software was forced upon us and we are very sad about it :(
+  </p>
 </template>
 
 <style>
--- src/views/ImpressumView.vue
+++ src/views/ImpressumView.vue
@@ -1,6 +1,7 @@
 <template>
   <h1>Impressum</h1>
-  Nookpost Developers
-  <br>
-  Contact: <a href="mailto:webmaster@jkulzer.dev">webmaster@jkulzer.dev</a>
+  <p>
+    Nookpost Developers
+    Contact: <a href="mailto:webmaster@jkulzer.dev">webmaster@jkulzer.dev</a>
+  </p>
 </template>
